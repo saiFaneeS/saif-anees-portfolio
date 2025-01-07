c25954d8a8bc7488b0adc83d937281430d3a4d48
Husban Baig <saifanees11@gmail.com>
2025-01-07 16:20:31 +0500

    first commit

--- components/TrustIndicators.jsx
+++ components/TrustIndicators.jsx
@@ -36,7 +36,7 @@ export const TrustIndicators = () => {
     <div className="flex items-center justify-center max-sm:flex-col gap-2 text-white-100/90 lg:bg-white lg:shadow lg:py-1 lg:px-4 rounded-full">
       <div className="flex items-center gap-1">
         {[...Array(5)].map((_, i) => (
-          <Star key={i} size={16} className="fill-yellow-500 text-yellow-500" />
+          <Star key={i} size={14} className="fill-yellow-500 text-yellow-500" />
         ))}
       </div>
       <motion.div
@@ -44,17 +44,49 @@ export const TrustIndicators = () => {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: -20 }}
-        className="text-base max-md:text-sm font-medium max-sm:text-center flex gap-2 items-center max-sm:justify-center flex-wrap"
+        className="text-sm max-md:text-sm font-medium max-sm:text-center flex gap-2 items-center max-sm:justify-center flex-wrap"
       >
         <span className="sm:mr-2">{testimonials[currentIndex]?.text}</span>
         <span className="text-sm text-white-100/60  flex gap-2 items-center">
-          —
-          <Image
-            src={testimonials[currentIndex]?.logo}
-            height={100}
-            width={100}
-            className="h-6 w-6 object-cover rounded-full"
-          />
+          —{" "}
+          {testimonials[currentIndex]?.author === "Tami Ross, Ohghad CEO" && (
+            <Image
+              src={testimonials[currentIndex]?.logo}
+              height={100}
+              width={100}
+              className="h-5 w-5 object-contain rounded-full"
+            />
+          )}
+          {testimonials[currentIndex]?.author === "Igal S." && (
+            <Image
+              src={testimonials[currentIndex]?.logo}
+              height={100}
+              width={100}
+              className="h-5 w-5 object-cover rounded-full"
+            />
+          )}
+          {/* {testimonials[currentIndex]?.author === "Violet Clough" && ( */}
+            <div className="bg-violet-600 p-[2px] rounded-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="15"
+                height="15"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#efefef"
+                stroke-width="1.8"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-flower-lotus"
+              >
+                <path d="M12 20c0-5.5-4.5-10-10-10 0 5.5 4.5 10 10 10" />
+                <path d="M9.7 8.3c-1.8-2-3.8-3.1-3.8-3.1s-.8 2.5-.5 5.4" />
+                <path d="M15 12.9V12c0-4.4-3-8-3-8s-3 3.6-3 8v.9" />
+                <path d="M18.6 10.6c.3-2.9-.5-5.4-.5-5.4s-2 1-3.8 3.1" />
+                <path d="M12 20c5.5 0 10-4.5 10-10-5.5 0-10 4.5-10 10" />
+              </svg>{" "}
+            </div>
+          {/* )} */}
           {testimonials[currentIndex]?.author}
         </span>
       </motion.div>
