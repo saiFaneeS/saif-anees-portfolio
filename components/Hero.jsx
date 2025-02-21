ed81ea7398660628576140e666e094a044ba1fda
Saifullah <121407595+saiFaneeS@users.noreply.github.com>
2025-02-21 09:52:12 +0000

    design updates

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,10 +5,17 @@ import React, { useEffect, useState } from "react";
 import Autoplay from "embla-carousel-autoplay";
 import { TrustIndicators } from "./TrustIndicators";
 import Slider from "./Slider";
+import Image from "next/image";
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 
 export default function Hero() {
   const { isActive, pageChanged, setPageChanged } = useNavbar();
   const [loading, setLoading] = useState(true);
+
+  const panel = [{ id: 1 }, { id: 2 }, { id: 3 }];
+  const images = [{ id: 1 }, { id: 2 }];
+
 
   useEffect(() => {
     setPageChanged(false);
@@ -22,9 +29,8 @@ export default function Hero() {
 
   return (
     <div
-      className={`h-screen overflow-x-hidden text-slate-950 max-sm:h-[88vh] flex flex-col justify-start max-sm:justify-between gap-8 max-sm:gap-4 px-24 max-lg:px-16 max-md:px-12 max-sm:px-4 pt-32 absolute w-full lg:overflow-hidden lg:h-screen ${
-        isActive ? "slide-out-top" : "slide-in-bottom"
-      } ${pageChanged ? "hidden" : ""}`}
+      className={`h-screen overflow-x-hidden text-slate-950 max-sm:h-[88vh] flex flex-col justify-start max-sm:justify-between gap-8 max-sm:gap-4 px-24 max-lg:px-16 max-md:px-12 max-sm:px-4 pt-32 absolute w-full lg:overflow-hidden lg:h-screen ${isActive ? "slide-out-top" : "slide-in-bottom"
+        } ${pageChanged ? "hidden" : ""}`}
     >
       <div className="text-6xl w-full max-sm:text-4xl pr-4 font-semibold leading-none">
         <div className="">
@@ -45,8 +51,59 @@ export default function Hero() {
         </div>
       </div>
 
+<div>
+
+      {/* works panel */}
+      <div className="flex gap-2 sm:hidden mb-4">
+        <div className="h-24 max-sm:h-20 aspect-[2.8] max-sm:w-40 rounded-md flex items-center justify-center gap-4 bg-neutral-300/50 overflow-hidden">
+          {images.map((el) => {
+            return (
+              <>
+                <div className="animate-snap-scroll snap-center snap-x snap-mandatory whitespace-nowrap h-20 max-sm:h-16 aspect-video animate-snap-loop">
+                  <Image
+                    src={"/ohghad-web.png"}
+                    height={200}
+                    width={200}
+                    className="h-full w-full object-cover rounded-md shadow-xl"
+                  />
+                </div>
+                <div className="animate-snap-scroll snap-center snap-x snap-mandatory whitespace-nowrap h-20 max-sm:h-16 aspect-video animate-snap-loop">
+                  <Image
+                    src={"/msre.png"}
+                    height={200}
+                    width={200}
+                    className="h-full w-full object-cover rounded-md shadow-xl"
+                  />
+                </div>
+                <div className="animate-snap-scroll snap-center snap-x snap-mandatory whitespace-nowrap h-20 max-sm:h-16 aspect-video animate-snap-loop">
+                  <Image
+                    src={"/mslm.webp"}
+                    height={200}
+                    width={200}
+                    className="h-full w-full object-cover rounded-md shadow-xl"
+                  />
+                </div>
+              </>
+            );
+          })}
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <h4 className="text-base font-medium leading-none mb-4">
+            My Works
+          </h4>
+          <Link
+            href={"/works"}
+            className="px-4 py-1 flex gap-2 items-center hover:underline text-sm text-nowrap"
+          >
+            <ArrowRight size={16} strokeWidth={1.5} /> Learn More
+          </Link>
+        </div>
+      </div>
+
+
       <div className="flex w-full gap-6 backdrop-blur-3xl max-sm:bg-emerald-800/[5%] max-sm:hover:bg-emerald-800/10 max-sm:border-2 border-emerald-900/20 max-sm:p-4 max-sm:rounded-md transition-all">
         <TrustIndicators />
+      </div>
       </div>
 
       <Slider />
